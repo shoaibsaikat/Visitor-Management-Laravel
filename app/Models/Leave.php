dcf4326685208f670e0bd083004b5f4b45ca6be3
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2023-10-19 14:30:06 +0600

    added leave model

---
+++ app/Models/Leave.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\People;
+
+class Leave extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+    ];
+}

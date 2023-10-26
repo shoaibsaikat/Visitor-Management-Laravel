aa7229abc5a012ab2ba600cf006a591757ccfb58
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2023-10-26 16:07:44 +0600

    ui fix

--- resources/views/officer/create.blade.php
+++ resources/views/officer/create.blade.php
@@ -24,6 +24,7 @@
         <div class="mt-4 pb-4">
             <label for="emails" class="block text-white font-bold">Select email address:</label>
             <select id="emails" name="email" class="w-full border rounded-md px-3 py-2 text-gray-700">
+                <option value="0">Select an email:</option>
                 @foreach ($users as $item)
                     <option value="{{$item->id}}">{{$item->email}}</option>
                 @endforeach

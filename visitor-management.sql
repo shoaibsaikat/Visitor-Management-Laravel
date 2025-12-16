551049b82dae3f29ced240310ffcf960a11fb137
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2025-12-16 14:43:46 +0600

    Update visitor-management.sql

--- visitor-management.sql
+++ visitor-management.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 11, 2023 at 12:19 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost
+-- Generation Time: Dec 16, 2025 at 08:43 AM
+-- Server version: 12.1.2-MariaDB
+-- PHP Version: 8.5.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -64,7 +64,11 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (8, '2023_10_03_083115_people_phone_unique', 4),
 (9, '2023_10_03_085956_visitor_history_id_type_change', 5),
 (12, '2023_10_03_090419_visitor_history_foreignkey_constraint', 6),
-(13, '2023_10_11_091425_can_manage_people_role', 7);
+(13, '2023_10_11_091425_can_manage_people_role', 7),
+(14, '2023_10_19_080307_people_user_relationship', 8),
+(15, '2023_10_19_081528_create_leave_table', 8),
+(16, '2023_10_29_094147_user_people_ralation', 8),
+(17, '2025_12_16_084158_drop_leaves_table', 9);
 
 -- --------------------------------------------------------
 
@@ -93,31 +97,32 @@ CREATE TABLE `people` (
   `address` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `nid` varchar(255) DEFAULT NULL,
-  `type` int(11) NOT NULL
+  `type` int(11) NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `people`
 --
 
-INSERT INTO `people` (`id`, `created_at`, `updated_at`, `name`, `designation`, `address`, `phone`, `nid`, `type`) VALUES
-(1, '2023-10-02 02:53:29', '2023-10-10 03:23:35', 'Rahman Subhan', 'Director', 'Agargaon', '1985648907', NULL, 0),
-(2, '2023-10-02 02:55:19', '2023-10-11 04:18:52', 'Rahim Didar', 'Director', 'Agargaon', '1567487705', NULL, 0),
-(3, '2023-10-02 04:20:40', '2023-10-10 03:23:54', 'Hakimuzzaman Khan', 'Deputy Director', 'Agargaon', '3675484568', NULL, 0),
-(4, '2023-10-02 22:43:11', '2023-10-08 02:40:52', 'Abdul Hamid', 'Deputy Director', 'Agargaon', '1234567891', NULL, 0),
-(19, '2023-10-04 04:09:20', '2023-10-04 04:09:20', 'Tipu', 'Pion', 'Kawran Bazar', '5674389219', NULL, 1),
-(21, '2023-10-05 02:19:31', '2023-10-05 02:19:31', 'Karim Molla', 'Delivery Man', 'Mirpur', '4658739876', NULL, 1),
-(22, '2023-10-08 02:36:22', '2023-10-08 02:36:22', 'Monir Khan', 'Delivery Man', 'Mirpur', '3567589487', NULL, 1),
-(23, '2023-10-08 04:22:17', '2023-10-08 04:22:17', 'Hamid Molla', 'Land Developer', 'Matuail', '4673829865', NULL, 1),
-(24, '2023-10-08 04:23:04', '2023-10-08 04:23:04', 'Asad Mahbub', 'CEO', 'Gulshan', '6473869500', NULL, 1),
-(25, '2023-10-08 04:23:45', '2023-10-08 04:23:45', 'Hamid Zaman', 'Marketing Director', 'Banani', '7584937865', NULL, 1),
-(26, '2023-10-08 04:24:20', '2023-10-08 04:24:20', 'Abida Sultana', 'CFO', 'Banani', '6589998765', NULL, 1),
-(27, '2023-10-08 04:25:01', '2023-10-08 04:25:01', 'Abdul Yusuf', 'Banker', 'Motijheel', '6655443322', NULL, 1),
-(28, '2023-10-08 04:25:35', '2023-10-08 04:25:35', 'Fahmida Anam', 'Director', 'Gulshan', '2343256789', NULL, 1),
-(29, '2023-10-08 04:26:08', '2023-10-08 04:26:08', 'Abdul Jabbar', 'Consultant', 'Mirpur', '5889765465', NULL, 1),
-(30, '2023-10-08 04:27:52', '2023-10-08 04:27:52', 'Mustafa Jabbar', 'Minister', 'Mirpur', '5643675890', NULL, 1),
-(31, '2023-10-08 04:28:42', '2023-10-08 04:28:42', 'Mahmuda Khanam', 'Banker', 'Motijheel', '3423567567', NULL, 1),
-(32, '2023-10-11 03:26:56', '2023-10-11 03:30:31', 'Hasna Hena', 'Deputy Director', 'Azimpur', '5664341768', NULL, 0);
+INSERT INTO `people` (`id`, `created_at`, `updated_at`, `name`, `designation`, `address`, `phone`, `nid`, `type`, `user_id`) VALUES
+(1, '2023-10-02 02:53:29', '2025-12-16 02:39:39', 'Rahman Subhan', 'Director', 'Agargaon', '1985648907', NULL, 0, 5),
+(2, '2023-10-02 02:55:19', '2023-10-11 04:18:52', 'Rahim Didar', 'Director', 'Agargaon', '1567487705', NULL, 0, NULL),
+(3, '2023-10-02 04:20:40', '2023-10-10 03:23:54', 'Hakimuzzaman Khan', 'Deputy Director', 'Agargaon', '3675484568', NULL, 0, NULL),
+(4, '2023-10-02 22:43:11', '2023-10-08 02:40:52', 'Abdul Hamid', 'Deputy Director', 'Agargaon', '1234567891', NULL, 0, NULL),
+(19, '2023-10-04 04:09:20', '2023-10-04 04:09:20', 'Tipu', 'Pion', 'Kawran Bazar', '5674389219', NULL, 1, NULL),
+(21, '2023-10-05 02:19:31', '2023-10-05 02:19:31', 'Karim Molla', 'Delivery Man', 'Mirpur', '4658739876', NULL, 1, NULL),
+(22, '2023-10-08 02:36:22', '2023-10-08 02:36:22', 'Monir Khan', 'Delivery Man', 'Mirpur', '3567589487', NULL, 1, NULL),
+(23, '2023-10-08 04:22:17', '2023-10-08 04:22:17', 'Hamid Molla', 'Land Developer', 'Matuail', '4673829865', NULL, 1, NULL),
+(24, '2023-10-08 04:23:04', '2023-10-08 04:23:04', 'Asad Mahbub', 'CEO', 'Gulshan', '6473869500', NULL, 1, NULL),
+(25, '2023-10-08 04:23:45', '2023-10-08 04:23:45', 'Hamid Zaman', 'Marketing Director', 'Banani', '7584937865', NULL, 1, NULL),
+(26, '2023-10-08 04:24:20', '2023-10-08 04:24:20', 'Abida Sultana', 'CFO', 'Banani', '6589998765', NULL, 1, NULL),
+(27, '2023-10-08 04:25:01', '2023-10-08 04:25:01', 'Abdul Yusuf', 'Banker', 'Motijheel', '6655443322', NULL, 1, NULL),
+(28, '2023-10-08 04:25:35', '2023-10-08 04:25:35', 'Fahmida Anam', 'Director', 'Gulshan', '2343256789', NULL, 1, NULL),
+(29, '2023-10-08 04:26:08', '2023-10-08 04:26:08', 'Abdul Jabbar', 'Consultant', 'Mirpur', '5889765465', NULL, 1, NULL),
+(30, '2023-10-08 04:27:52', '2023-10-08 04:27:52', 'Mustafa Jabbar', 'Minister', 'Mirpur', '5643675890', NULL, 1, NULL),
+(31, '2023-10-08 04:28:42', '2023-10-08 04:28:42', 'Mahmuda Khanam', 'Banker', 'Motijheel', '3423567567', NULL, 1, NULL),
+(32, '2023-10-11 03:26:56', '2023-10-11 03:30:31', 'Hasna Hena', 'Deputy Director', 'Azimpur', '5664341768', NULL, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -153,16 +158,17 @@ CREATE TABLE `users` (
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `can_manage_people` tinyint(1) NOT NULL DEFAULT 0
+  `can_manage_people` tinyint(1) NOT NULL DEFAULT 0,
+  `officer_id` bigint(20) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `can_manage_people`) VALUES
-(4, 'Developer', 'dev@dev.com', NULL, '$2y$10$ufzU5kYZg4deqvh6SyRf8.gPEzQ3BjwgzQkO8TpmxGXWNz5X0C2YW', NULL, '2023-10-11 02:20:59', '2023-10-11 02:20:59', 1),
-(5, 'Guest', 'guest@dev.com', NULL, '$2y$10$Y3rz9hQzs/Gs8LekEzc0Q./bQd5/I7FzHZbhk2.Znyy5ySP9venlO', NULL, '2023-10-11 03:28:13', '2023-10-11 03:28:13', 0);
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `can_manage_people`, `officer_id`) VALUES
+(4, 'Developer', 'dev@dev.com', NULL, '$2y$10$ufzU5kYZg4deqvh6SyRf8.gPEzQ3BjwgzQkO8TpmxGXWNz5X0C2YW', 'wm7cp7ChGOhyLDD07it2BMW3n3p6lFR42GiaRvtK9Aq6BLWQYTfLAYJRmMAM', '2023-10-11 02:20:59', '2023-10-11 02:20:59', 1, NULL),
+(5, 'Guest', 'guest@dev.com', NULL, '$2y$10$Y3rz9hQzs/Gs8LekEzc0Q./bQd5/I7FzHZbhk2.Znyy5ySP9venlO', NULL, '2023-10-11 03:28:13', '2025-12-16 02:39:39', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -231,7 +237,8 @@ ALTER TABLE `password_reset_tokens`
 --
 ALTER TABLE `people`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `people_phone_unique` (`phone`);
+  ADD UNIQUE KEY `people_phone_unique` (`phone`),
+  ADD KEY `people_user_id_foreign` (`user_id`);
 
 --
 -- Indexes for table `personal_access_tokens`
@@ -246,7 +253,8 @@ ALTER TABLE `personal_access_tokens`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD KEY `users_officer_id_foreign` (`officer_id`);
 
 --
 -- Indexes for table `visitor_histories`
@@ -270,7 +278,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `people`
@@ -299,6 +307,18 @@ ALTER TABLE `visitor_histories`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `people`
+--
+ALTER TABLE `people`
+  ADD CONSTRAINT `people_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_officer_id_foreign` FOREIGN KEY (`officer_id`) REFERENCES `people` (`id`);
 
 --
 -- Constraints for table `visitor_histories`
